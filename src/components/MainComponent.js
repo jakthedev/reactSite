89e86217e0ec8dc272f72f273aac4090badc8052
Jd Kemp <jdakemp@gmail.com>
2021-02-03 21:16:43 -0500

    defined the state in the main component more and turn the directory component into more of a presentational component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './DirectoryComponent';
+import  CampsiteInfo  from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
 
 class Main extends Component { 
   constructor(props) {
     super(props); 
     this.state = {
-      campsites: CAMPSITES
+      campsites: CAMPSITES,
+      selectedCampsite:null
     };
   }
+
+  onCampsiteSelect(campsiteid) {
+    this.setState({selectedCampsite: campsiteid});
+}
+
   render() {
       return (
           <div >
@@ -18,7 +25,8 @@ class Main extends Component {
                   <NavbarBrand href="/">NuCamp</NavbarBrand>
               </div>
               </Navbar>
-              <Directory campsites={this.state.campsites}/>
+              <Directory campsites={this.state.campsites} onClick={campsiteid => this.onCampsiteSelect(campsiteid)}/>
+              <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
           </div>
       );
   }
