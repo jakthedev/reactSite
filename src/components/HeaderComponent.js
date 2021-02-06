59512a509ceb8fd2f5ebb71b210a5d137245c413
Jd Kemp <jdakemp@gmail.com>
2021-02-06 13:21:50 -0500

    add navgatation links to the navbar and also made the navbar responsive with reactstrap and reactrouter

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,7 +1,22 @@
 import React, { Component }  from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.toggleNav = this.toggleNav.bind(this);
+        this.state = {
+            isNavOpen: false
+        }
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
     render() {
         return (
             <React.Fragment>
@@ -16,9 +31,11 @@ class Header extends Component {
                     </div>
                 </Jumbotron>
 
-                <Navbar dark sticky="top">
+                <Navbar dark sticky="top" expand="md">
                     <div className="container">
-                        <NavbarBrand href="/">NuCamp</NavbarBrand>
+                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/logo.png" height="30"
+                        width="30" alt="Nucamp Logo" /></NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} />
                     </div>
                 </Navbar>
             </React.Fragment>
