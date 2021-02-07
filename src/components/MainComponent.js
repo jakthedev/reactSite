df25ca62daa751d4726003d80e2f1742bb300347
Jd Kemp <jdakemp@gmail.com>
2021-02-07 17:40:44 -0500

    footer is working great with reactRouter, still dont know why the page links in the navbar are not working, but everything else is working.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent'; 
 import Footer from './FooterComponent'; 
 import Home from './HomeComponent'; 
 import { Switch, Route, Redirect } from 'react-router-dom';
-import  CampsiteInfo  from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
 
 class Main extends Component { 
+
   constructor(props) {
     super(props); 
     this.state = {
-      campsites: CAMPSITES,
+      campsites: CAMPSITES
       
     };
   }
