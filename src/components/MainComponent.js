864e1121b8522362cac89fe63f573de2a7fb35e2
Jd Kemp <jdakemp@gmail.com>
2021-02-07 18:27:42 -0500

    contact us view works

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,8 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent'; 
 import Footer from './FooterComponent'; 
-import Home from './HomeComponent'; 
+import Home from './HomeComponent';  
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 
@@ -30,8 +31,9 @@ class Main extends Component {
               <Header /> 
               <Switch>
                 <Route path='/home' component={HomePage} /> 
-                <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites}
-                />} />
+                <Route exact path='/directory' render={() => 
+                <Directory campsites={this.state.campsites} />} />
+                <Route exact path='/contactus' component={Contact} />
               <Redirect to='/home' />
               </Switch>
             
