4b3307e6b041cd1b619b26a66984e209be998421
Jd Kemp <jdakemp@gmail.com>
2021-02-08 08:42:39 -0500

    Set up the state for the home component pg in the Main component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,14 +6,20 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';  
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import { CAMPSITES } from '../shared/campsites';
+import { CAMPSITES } from '../shared/campsites'; 
+import { COMMENTS } from '../shared/comments'; 
+import { PARTNERS } from '../shared/partners'; 
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component { 
 
   constructor(props) {
     super(props); 
     this.state = {
-      campsites: CAMPSITES
+      campsites: CAMPSITES, 
+      comments: COMMENTS, 
+      partners: PARTNERS, 
+      promotions: PROMOTIONS
       
     };
   }
@@ -23,7 +29,11 @@ class Main extends Component {
   render() {
       const HomePage = () => {
         return (
-            <Home />
+            <Home 
+              campsite={this.state.campsites.filter(campsite => campsite => campsite.featured)[0]}
+              promotion={this.state.promotions.filter(promotion => promotion.featured.featured)[0]}
+              partner={this.state.partners.filter(partner => partner.featured)[0]}
+            />
         );
       }
       return (
